61cd2d3696acedb99980ad86e6202e47728da1bb
alok <ay879018@gmail.com>
2026-03-04 23:15:30 +0530

    added MutationObserver to detect dynamically loaded forms

--- content.js
+++ content.js
@@ -1,49 +1,72 @@
-console.log("Form filler is running");
+function fillforms(){
 
-//finding all inputs in the form 
+    //finding all inputs in the form 
 
-const inputs = document.querySelectorAll("input, textarea");
+    const inputs = document.querySelectorAll("input, textarea");
 
-console.log("Detected inputs:", inputs);
+    console.log("Detected inputs:", inputs);
 
-//looping thorugh every inputs to know there input type
-inputs.forEach((input) => {
-   
-    const fieldInfo = (
-        (input.name || "") +
-        (input.id || "") +
-        (input.placeholder || "")
-    ).toLowerCase();
-
-
-    //to handle labels 
-
-    let labelText = "";
-    if (input.labels && input.labels.length >0) {
-        lableText = input.labels[0].innerText.toLowerCase();
-    }
-
+    //looping thorugh every inputs to know there input type
+    inputs.forEach((input) => {
     
-    const combinedInfo = fieldInfo + labelText;
+        const fieldInfo = (
+            (input.name || "") +
+            (input.id || "") +
+            (input.placeholder || "")
+        ).toLowerCase();
 
-    
-    if (combinedInfo.includes("email")) {
-        input.value = "ay879018@gmail.com";
 
-        //webiste made from react angular does not allow direct value changes so we trigger an input event 
-        //it triggers an input event taht user typed something
+        //to handle labels 
+
+        let labelText = "";
+        if (input.labels && input.labels.length >0) {
+            lableText = input.labels[0].innerText.toLowerCase();
+        }
+
         
-        input.dispatchEvent(new Event("input", { bubbles: true}));
-    }
+        const combinedInfo = fieldInfo + labelText;
 
-    if (fieldInfo.includes("name")) {
-        input.value = "Alok Yadav";
-        input.dispatchEvent(new Event("input", { bubbles: true}));
-    }
+        
+        if (combinedInfo.includes("email")) {
+            input.value = "ay879018@gmail.com";
 
-    if (fieldInfo.includes("phone")) {
-        input.value = "9682818068" ;
-        input.dispatchEvent(new Event("input", { bubbles: true}));
-    }
+            //webiste made from react angular does not allow direct value changes so we trigger an input event 
+            //it triggers an input event taht user typed something
+            
+            input.dispatchEvent(new Event("input", { bubbles: true}));
+        }
 
+        if (fieldInfo.includes("name")) {
+            input.value = "Alok Yadav";
+            input.dispatchEvent(new Event("input", { bubbles: true}));
+        }
+
+        if (fieldInfo.includes("phone")) {
+            input.value = "9682818068" ;
+            input.dispatchEvent(new Event("input", { bubbles: true}));
+        }
+
+    });
+}
+
+//function call 
+fillforms();
+
+
+//creating a mutation observer - whenever the DOM changes it will run fillforms()
+
+const observer = new MutationObserver(() => {
+    fillforms();
 });
+
+
+//telling observer to watch changes in DOM tree
+// document.body -> tells observer to watch from the body so it watches whole webpage
+//childList -> watches of new dic , labels
+//subtree -> watches all nested elements inside body 
+
+observer.observe(document.body, {
+    childList: true,
+    subtree: true
+});
+
